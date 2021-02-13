24ce369a53b6d9fa85ca77293cf39bf580fdd7be
ABK <50417542+Klaus651@users.noreply.github.com>
2021-02-13 21:07:37 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6027f1f26c1e89001ea0d2ff",
+				"6027f1f26c1e89001ea0d301"
+			]
+		},
+		"6027f1f26c1e89001ea0d2ff": {
+			"id": "6027f1f26c1e89001ea0d2ff",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6027f1f26c1e89001ea0d301": {
+			"id": "6027f1f26c1e89001ea0d301",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
